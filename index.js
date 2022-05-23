a47dcfc9a453d71e30bc08e0dd3b52da0c60b608
Lucas Sweeney <lsweeney0124@gmail.com>
2022-05-23 11:07:52 -0600

    commit tree 5fab3492349d84f6bf3e371d728f997b238c9d16

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-function addingEventListener() {
+function addingEventListener () {
+  const input = document.getElementById('input');
+
+  function clickAlert () {
+    alert('I was Clicked!');
+  }
+  input.addEventListener('click', clickAlert)
 }
+
+addingEventListener()
+
+
